b86f3c16abc0f88560ce67d8d5d801ad22191133
Yusuf Erdem <yusuf@thundra.io>
2021-11-06 09:26:12 +0300

    skip

---
+++ src/test/java/com/yourcompany/Tests/SkipExample.java
@@ -0,0 +1,23 @@
+package com.yourcompany.Tests;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class SkipExample {
+    @Test(enabled=false)
+    public void testCaseEnabling(){
+        System.out.println("I'm Not Ready, please don't execute me");
+    }
+    @Test
+    public void testCaseSkipException(){
+        System.out.println("Im in skip exception");
+        throw new SkipException("Skipping this exception");
+    }
+
+    @Test
+    public void testCaseConditionalSkipException(){
+        System.out.println("Im in Conditional Skip");
+        if(!false)
+            throw new SkipException("Skipping this exception");
+        System.out.println("Executed Successfully");
+    }
+}
