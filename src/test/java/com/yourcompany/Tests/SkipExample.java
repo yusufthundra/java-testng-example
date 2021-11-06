5a30f4c20b1ce1f40f64799d6b8e30a8c536c982
Yusuf Erdem <yusuf@thundra.io>
2021-11-06 09:35:40 +0300

    skip update

--- src/test/java/com/yourcompany/Tests/SkipExample.java
+++ src/test/java/com/yourcompany/Tests/SkipExample.java
@@ -1,22 +1,23 @@
 package com.yourcompany.Tests;
 import org.testng.SkipException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class SkipExample {
-    @Test(enabled=false)
+public class SkipExample extends TestBase{
+    @Test(enabled=false, dataProvider = "hardCodedBrowsers")
     public void testCaseEnabling(){
         System.out.println("I'm Not Ready, please don't execute me");
     }
-    @Test
+    @Test(dataProvider = "hardCodedBrowsers")
     public void testCaseSkipException(){
         System.out.println("Im in skip exception");
         throw new SkipException("Skipping this exception");
     }
 
-    @Test
+    @Test (dataProvider = "hardCodedBrowsers")
     public void testCaseConditionalSkipException(){
         System.out.println("Im in Conditional Skip");
-        if(!false)
+        if(!"asd".equals("fff"))
             throw new SkipException("Skipping this exception");
         System.out.println("Executed Successfully");
     }
